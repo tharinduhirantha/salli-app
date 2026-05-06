f5699c84c27c616ff5afadb48eed69ca913eea0a
Tharindu Hirantha <tharindu@Tharindus-MacBook-Pro.local>
2026-05-05 23:07:03 -0500

    Bump version to v1.1.8

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -105,7 +105,7 @@ export default function SettingsScreen() {
           <Text style={s.signOutText}>Sign Out</Text>
         </TouchableOpacity>
 
-        <Text style={s.version}>Salli v1.1.7</Text>
+        <Text style={s.version}>Salli v1.1.8</Text>
       </ScrollView>
     </ScreenWrapper>
   );
